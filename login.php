bc4390a85221912e73323e6657efd8deff3d9c14
Mohammed Ali <mohammedyoussef.al@gmail.com>
2022-06-14 21:45:58 +0200

    update

--- login.php
+++ login.php
@@ -2,11 +2,30 @@
 <html>
 
 <head>
-
+    <meta charset="UTF-8">
+    <!----css link viewport----->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!----css link nav-footer homepage----->
+    <link rel="stylesheet" href="./css/nav-footer.css" />
+    <!----css link info----->
+    <link rel="stylesheet" href="./css/info.css" />
+    <!----css link skin color----->
+    <link rel="stylesheet" href="./skin/color-1.css">
+    <!-- Font icons pulled from remix icon CDN (Content delivery network) -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <title>Login</title>
+    <?php
+            require_once 'layout.php';
+            $layout = new PageLayout();
+        ?>
 </head>
 
 <body>
+    <!-- navigation menu start -->
+    <?php
+            $layout->getNavbarHead("");
+        ?>
+    <!-- navigation menu end  -->
     <div id="main">
         <h1>Login</h1>
         <form method="POST">
@@ -15,6 +34,11 @@
             <input type="submit" name="submit" id="sub">
         </form>
     </div>
+    <!-- Footer Contact start -->
+    <?php
+            $layout->getNavbarFoot("");
+        ?>
+    <!-- Footer Contact end -->
 </body>
 
 </html>
--- reservatie.php
+++ reservatie.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html>
 
@@ -11,8 +9,8 @@
     <link rel="stylesheet" href="./css/nav-footer.css" />
     <!----css link skin color----->
     <link rel="stylesheet" href="./skin/color-1.css">
-    <!--font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Font icons pulled from remix icon CDN (Content delivery network) -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <title>Verassend Barcelona</title>
     <?php
             require_once 'layout.php';
@@ -28,62 +26,62 @@
     <!-- navigation menu end  -->
 
     </head>
+
     <body>
-    <div class="rent">
-        <br><br>
-    </div>
-    <div class="box1"><h1>
-            Reserveer hier uw Fiets (tour)
-    </h1>
-        <label for="lname">Naam </label>
-    <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
-  
-    <label for="lname">Adres </label>
-    <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
+        <div class="rent">
+            <br><br>
+        </div>
+        <div class="box1">
+            <h1>
+                Reserveer hier uw Fiets (tour)
+            </h1>
+            <label for="lname">Naam </label>
+            <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
 
-    <label for="lname">E-mail </label>
-    <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
-    
-    <label for="lname">IBAN </label>
-    <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
- 
-    
-    <h2> Maak hier uw keuze! </h2>
-    <label class="container">Fiets tour (T.w.v.€85,- )
-  <input type="radio" checked="checked" name="radio">
-  <span class="checkmark"></span>
-</label>
-<label class="container">Fiets huren (T.w.v.€45,-)
-  <input type="radio" name="radio">
-  <span class="checkmark"></span>
-</label>
-   
+            <label for="lname">Adres </label>
+            <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
 
-    </div>
-<br>
-    <div class="box2">
-        <h2>Prijzen </h2>
-        <p>In verband met stijgende prijzen hanteren wij een minimum van max. 8 mensen per bestelling* </p><br><br>
-        <h3>Fiets tour  </h3><br>
-        <h3>€ 85,- </h3><br><br>
+            <label for="lname">E-mail </label>
+            <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
 
-        <h3> Fiets huren per stuk </h3><br><br>
-        <h3>€ 45,- <h3><br>
+            <label for="lname">IBAN </label>
+            <input type="text" id="lname" name="lname" value=" " size="45"><br><br>
 
-    </div>
-<div class="img">
-<img src="bikeride.jpg" alt="bikeride" height="600" width="370">
-</div>
-<div class="img2">
-    <img src="bikerack.jpeg" alt="bikerack" height="600" width="370">
-</div>
-    </body>
-</html>
-    <!-- Footer Contact start -->
-    <?php
+
+            <h2> Maak hier uw keuze! </h2>
+            <label class="container">Fiets tour (T.w.v.€85,- )
+                <input type="radio" checked="checked" name="radio">
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">Fiets huren (T.w.v.€45,-)
+                <input type="radio" name="radio">
+                <span class="checkmark"></span>
+            </label>
+
+
+        </div>
+        <br>
+        <div class="box2">
+            <h2>Prijzen </h2>
+            <p>In verband met stijgende prijzen hanteren wij een minimum van max. 8 mensen per bestelling* </p><br><br>
+            <h3>Fiets tour </h3><br>
+            <h3>€ 85,- </h3><br><br>
+
+            <h3> Fiets huren per stuk </h3><br><br>
+            <h3>€ 45,- <h3><br>
+
+        </div>
+        <div class="img">
+            <img src="bikeride.jpg" alt="bikeride" height="600" width="370">
+        </div>
+        <div class="img2">
+            <img src="bikerack.jpeg" alt="bikerack" height="600" width="370">
+        </div>
+        <!-- Footer Contact start -->
+        <?php
             $layout->getNavbarFoot("");
         ?>
-    <!-- Footer Contact end -->
-</body>
+        <!-- Footer Contact end -->
+    </body>
 
 </html>
